7490b6f2d8efd0e03bbc0cf5ef90d98899c730c1
karasu yami <demigodlinty@gmail.com>
2023-02-21 10:36:49 -0600

    added eslint

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,9 +1,55 @@
-const {User, bookSchema} = require('../models');
 
+const {AuthenticationError} = require('apollo-server-express')
+const {User, BookSchema} = require('../models');
+const {signToken} = require('../utils/auth')
 const resolvers = {
     Query: {
-        book: async () => {
-            return bookSchema.find({});
+        users: async () => {
+            return User.find({}).populate('book')
+        },
+        user: async (parent, {username}) => {
+            return User.findOne({user}).populate('user')
+        },
+        books: async (parent, {username}) => {
+            const params = username ? {username} : {};
+            return bookSchema.find(params).sort({createdAt: -1});
+    },
+    book: async (parent, {username}) => {
+        return BookSchema.findOne({_id: bookId})
+    }
+},
+Mutation: {
+    newUser: async (parent, {email, password}) => {
+        const user = await User.create({username, email, password})
+        const token = signToken(user)
+        return  {user, token}
+    },
+    login: async (parent,{email, password}) => {
+         const user = await User.findOne({email});
+         if (!user) {
+            throw new AuthenticationError('no sucj user!....try again?')
+         }
+         const passwordnerify = await user.isCorrectPassword(password);
+         if(!passwordnerify){
+            throw AuthenticationError('wrong password')
+         }
+         const token = signToken(user);
+        
+         return {token, user};
+    },
+
+    addBook: async (parent, {user}) => {
+        const updatedUser = await User.findByIdAndUpdate(
+                { _id: user._id },
+                { $addToSet: { savedBooks: body } },
+                { new: true, runValidators: true }
+            )
+            return updatedUser
         }
+    },
+    removeBook: async(parent, {bookId}) => {
+        return BookSchema.findOneAndDelete({_id: bookId})
     }
 }
+
+module.exports = resolvers;
--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -17,5 +17,13 @@ type User {
     password: string!
     savedBooks: [bookSchema]!
 }
+
+  type Mutation {
+    newUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addBook(thoughtText: String!): Book
+    removeBook(thoughtId: ID!): Book
+
+  }
 `
 module.exports = typeDefs
