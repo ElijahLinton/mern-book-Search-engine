1071572f131f7cb40b376171fd07977d324ebf75
karasu yami <demigodlinty@gmail.com>
2023-02-26 20:04:31 -0600

    added changes to the frontend

--- Develop/client/src/pages/SearchBooks.js
+++ Develop/client/src/pages/SearchBooks.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import {useMutation} from '@apollo/client';
+import {GET_ME} from '../utils/queries';
+import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
